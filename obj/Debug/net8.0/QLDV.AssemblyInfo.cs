26af2b0c9e15a733eaf544788517efd7dda83682
Gia Huy <teongu2210@gmail.com>
2026-05-22 11:38:50 +0700

    22/5

--- obj/Debug/net8.0/QLDV.AssemblyInfo.cs
+++ obj/Debug/net8.0/QLDV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLDV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ebb3804fa915ab3a57abb93ed153632bf200ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6ce936e8e254e0fe12ac5c7d43d19577b847cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLDV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLDV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
